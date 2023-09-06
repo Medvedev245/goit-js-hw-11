39cb22b5f77abe7e0aeeb0746e538df20189ceb1
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2023-09-06 22:58:54 +0200

    all done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-/**********refs**********/
-import { servicePhoto, perPage } from './service';
-console.log(servicePhoto);
-
+/*********************imports*********************/
+import SimpleLightbox from 'simplelightbox';
 import Notiflix from 'notiflix';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { servicePhoto, perPage } from './service';
 
+/***********************refs**********************/
 console.log(perPage);
 const elements = {
   container: document.querySelector('.gallery'),
   tgt: document.querySelector('.search-form-js'),
   btnLoad: document.querySelector('.js-load-more'),
 };
+const gallery = document.querySelector('.gallery');
 let page = 1;
 let textInput = '';
 
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 elements.tgt.addEventListener('submit', hendlerSubmit);
 elements.btnLoad.addEventListener('click', handlerLoadMore);
+
+/********************functions********************/
 
 function handlerLoadMore() {
   page += 1;
@@ -24,6 +33,7 @@ function handlerLoadMore() {
       elements.container.insertAdjacentHTML('beforeend', createMurcup(data));
       if (data.totalHits / perPage <= page) {
         elements.btnLoad.classList.add('load-more-hidden');
+        Notiflix.Notify.info('Sorry bro, its last page ):');
       }
       console.log(data);
     })
@@ -36,14 +46,14 @@ function hendlerSubmit(evt) {
   textInput = evt.currentTarget.elements.searchQuery.value;
   servicePhoto(1, textInput)
     .then(response => {
+      if (response.total === 0) {
+        Notiflix.Notify.failure('Sorry, there are no images.please try again');
+      }
       console.log(response);
       const el = response.hits;
-      // console.log(el);
-
       return createMurcup(response);
     })
     .catch(err => console.log(err));
-  // return evt.currentTarget.elements.searchQuery.value
 }
 
 function createMurcup(array) {
@@ -63,27 +73,28 @@ function createMurcup(array) {
       <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300" height="200" />
       <div class="info">
         <p class="info-item">
-          <b>Likes${likes}</b>
+          <b>Likes: </b><span class="numbers">${likes}</span>
         </p>
         <p class="info-item">
-          <b>Views${views}</b>
+          <b>Views: </b><span class="numbers">${views}</span>
         </p>
         <p class="info-item">
-          <b>Comments${comments}</b>
+          <b>Comments: </b><span class="numbers">${comments}</span>
         </p>
         <p class="info-item">
-          <b>Downloads${downloads}</b>
+          <b>Downloads: </b><span class="numbers">${downloads}</span>
         </p>
       </div>
       </a>
     </div>
-    
     `;
     })
     .join('');
 
   elements.container.insertAdjacentHTML('beforeend', resp);
+  lightbox.refresh();
   if (array.totalHits > perPage) {
+    Notiflix.Notify.success('Hurray, we found 500 images');
     elements.btnLoad.classList.replace('load-more-hidden', 'load-more');
   }
 }
